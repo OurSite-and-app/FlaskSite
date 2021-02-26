50d39a14a17496a85d3d771aeff6d60c26a0f9a4
OurSite-and-app <gasanov.ea@edu.spbstu.ru>
2021-02-26 19:39:25 +0300

    ping via json and err

--- TestApi/app.py
+++ TestApi/app.py
@@ -14,7 +14,9 @@ def getProducts():
 @app.route('/products/<string:product_name>') #хотим получить часть запроса
 def getProduct(product_name):
     productsFound=[product for product in products if product['name']== product_name]
-    return jsonify({"product": productsFound[0]})
+    if (len(productsFound)>0):
+        return jsonify({"product": productsFound[0]})
+    return jsonify({"message":"Product not found"})
 
 
 
--- TestApi/products.py
+++ TestApi/products.py
@@ -1,6 +1,6 @@
 products = [
     {"name": "laptop", "price":800,"quantity":4},
     {"name": "mouse", "price":40,"quantity":10},
-    {"name": ",omitor", "price":400,"quantity":3},
+    {"name": "monitor", "price":400,"quantity":3},
 
 ] # данные которые будут доставлять клиентские приложения
