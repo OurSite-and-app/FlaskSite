a8d504607b8a2cd52cccb4996c54e3ab52906445
OurSite-and-app <gasanov.ea@edu.spbstu.ru>
2021-02-26 17:35:46 +0300

    ping via json

--- TestApi/products.py
+++ TestApi/products.py
@@ -1,6 +1,6 @@
-product = [
-    {"name": "laptop", "price":800,:"quantity":4},
-    {"name": "mouse", "price":40,:"quantity":10},
-    {"name": ",omitor", "price":400,:"quantity":3},
+products = [
+    {"name": "laptop", "price":800,"quantity":4},
+    {"name": "mouse", "price":40,"quantity":10},
+    {"name": ",omitor", "price":400,"quantity":3},
 
 ] # данные которые будут доставлять клиентские приложения
