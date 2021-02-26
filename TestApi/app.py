183d678d0e58c30a8d05faa871df913b0b57f6bb
OurSite-and-app <gasanov.ea@edu.spbstu.ru>
2021-02-26 21:21:56 +0300

    method post added

--- TestApi/app.py
+++ TestApi/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app=Flask(__name__)
 
 from products import products
@@ -19,6 +19,16 @@ def getProduct(product_name):
     return jsonify({"message":"Product not found"})
 
 
+@app.route('/products',methods=['POST'])
+def addProduct():
+    #print(request.json)
+    new_product={
+        "name":request.json["name"],
+        "price":request.json["price"],
+        "quantity":request.json["quantity"]
+    }
+    products.append(new_product)
+    return jsonify({"message":"Product added succsesfully","products are":products})
 
 if __name__ =='__main__':
     app.run(debug=True,port=4000)
