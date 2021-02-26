0ad870e746572ccefe0ee12494a24b15fac28275
OurSite-and-app <gasanov.ea@edu.spbstu.ru>
2021-02-26 18:13:08 +0300

    products via json

--- TestApi/app.py
+++ TestApi/app.py
@@ -3,9 +3,14 @@ app=Flask(__name__)
 
 from products import products
 
-@app.route('/ping')
+@app.route('/ping', methods=['GET'])
 def ping():
     return jsonify({"message": "pong!"})
+
+@app.route('/products')
+def getProduct():
+    return jsonify(products)
+
 
 
 if __name__ =='__main__':
